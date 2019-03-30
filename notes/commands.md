cd62fc8d29a09509c61df7b52941db041dbccad9
Bell Eapen <github_public@gulfdoctor.net>
2019-03-30 19:52:13 -0400

    Begin test files

---
+++ notes/commands.md
@@ -0,0 +1,10 @@
+
+```
+ python qrmine.py -i transcript.txt --codedict -n 5
+ 
+ python qrmine.py -i transcript.txt --cat -n 20
+  
+ python qrmine.py -i transcript.txt --topics
+ 
+  
+```
---
+++ tests/test_nlp.py
@@ -0,0 +1,25 @@
+import io
+import unittest
+from src.nlp_qrmine import Qrmine, ReadData, Content
+
+
+class NLPTest(unittest.TestCase):
+
+    def setUp(self):
+        output = io.StringIO()
+        output.write('First line.\n')
+
+        self.q = Qrmine()
+        self.data = ReadData()
+        self.data.read_file(output)
+        self.interviews = Content(self.data.content)
+
+    def test_something(self):
+        self.assertEqual(True, True)
+
+    def test_codedict(self):
+        self.q.print_dict(self.interviews, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
