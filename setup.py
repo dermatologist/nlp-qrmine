3e43c3b22e1ef7c42965d7010297a597e093506e
Bellraj Eapen <github@gulfdoctor.net>
2021-07-17 07:24:22 -0400

    after autoupdate

--- setup.py
+++ setup.py
@@ -1,32 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """
     Setup file for qrmine.
+    Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 3.0.3.
+    This file was generated with PyScaffold 4.0.2.
     PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: http://pyscaffold.org/
+    Learn more under: https://pyscaffold.org/
 """
-
-import sys
 from setuptools import setup
 
-# Add here console scripts and other entry points in ini-style format
-entry_points = """
-[console_scripts]
-qrmine = qrmine.main:main_routine
-# For example:
-# fibonacci = qrmine.skeleton:run
-"""
-
-
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['pyscaffold>=3.0a0,<3.1a0'] + sphinx,
-          entry_points=entry_points,
-          use_pyscaffold=True)
-
-
 if __name__ == "__main__":
-    setup_package()
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
