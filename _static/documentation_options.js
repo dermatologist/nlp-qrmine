10580d27c8c41eb3c6dc8627bd99492af113e943
dermatologist <dermatologist@users.noreply.github.com>
2024-11-10 20:22:26 +0000

    Deploying to gh-pages from @ dermatologist/nlp-qrmine@accf1a4a7623a8514030f9b263f4369b22eb6e0b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+const DOCUMENTATION_OPTIONS = {
     VERSION: 'unknown',
-    LANGUAGE: 'None',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
